29477655c83b9849645a3b80f0096090f60971e7
Iain Rough <roughiain@gmail.com>
2023-08-17 08:22:34 +0200

    Add xaml with no linebreaks

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -19,11 +19,12 @@ public partial class MainPage : ContentPage
 		</Label>
 		""";
 
+    private static string xamlWithNoLineBreaks = "<Label LineBreakMode=\"WordWrap\"><Label.FormattedText><FormattedString><Span Text=\"Red Bold, \" TextColor=\"Red\" FontAttributes=\"Bold\" /><Span Text=\"default, \" FontSize=\"14\"/><Span Text=\"italic small.\" FontAttributes=\"Italic\" FontSize=\"12\" /></FormattedString></Label.FormattedText></Label>";
 
     public MainPage()
 	{
 		InitializeComponent();
-        var label = new Label().LoadFromXaml(xaml);
+        var label = new Label().LoadFromXaml(xamlWithNoLineBreaks);
         if (label.FormattedText is null)
         {
             throw new Exception("This does not work");
